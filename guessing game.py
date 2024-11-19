391d79e66d5641d553db92c316d165a6d359f404
anastasia-anufrikova <nastyaanufrikova1996@gmail.com>
2024-11-19 15:06:58 +0300

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -1,14 +1,24 @@
 import random
+x = random.randint(1,100)
+print('Добро пожаловать в числовую угадайку')
+def is_valid(a):
+    return a.isdigit() and 1 <= int(a) <= 100
 
-x = random.randint(1, 100)
-print('Введите число')
-a = int(input())
-while a != x:
-    if a > x:
-        print('Слишком много, попробуйте еще раз')
-        a = int(input())
-    elif a < x:
-        print('Слишком мало, попробуйте еще раз')
-        a = int(input())
-if a == x:
+a = input()
+while x != a:
+    check = is_valid(a)
+    if check == False:
+        print('А может быть все-таки введем целое число от 1 до 100?')
+        a = input()
+    elif check == True:
+        if int(a) > x:
+            print('Слишком много, попробуйте еще раз')
+            a = input()
+        elif int(a) < x:
+            print('Слишком мало, попробуйте еще раз')
+            a = int(input())
+        elif int(a) == x:
+            break
+if int(a) == x:
     print('Вы угадали, поздравляем!')
+    print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
